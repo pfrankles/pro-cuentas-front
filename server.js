0b64048d24bdc0bc92245f341da7e9e2316bb816
pfrankles <70445253+pfrankles@users.noreply.github.com>
2020-12-15 21:21:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.get(/.*/, function (req, res) {
 res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
