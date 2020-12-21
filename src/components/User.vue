b748dba9fac8b64d41b5f8970d84fea3d478d9c8
pfrankles <70445253+pfrankles@users.noreply.github.com>
2020-12-20 20:51:33 -0500

    Update User.vue

--- src/components/User.vue
+++ src/components/User.vue
@@ -7,6 +7,10 @@
         <br>
         <button v-on:click="salir" id="cerrar"> Cerrar Sesión </button>
     <br>
+    <br>
+    <iframe name="iframe1" id="iframe1" title="APP" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_AKrWTvCjzSnCuFby5nYzyJ3LfJ6DhoHfZ3T9Bc1LZEdv4grrVh_pZOHxE0qUXw/pubhtml?widget=true&amp;headers=false"
+        frameborder="" border="0" cellspacing="0"
+        style="border-style: none;width: 99%; height: 660px;"></iframe>
     </div>
 
 </template>
@@ -65,22 +69,30 @@
 
 <style>
     #User{
-        width: 50%;
-        height: 10%;
         display: flex;
         justify-content: center;
         align-items: center;
-        width:600px;height:600px;
+        width:147%;height:350px;
+        position:relative;
+        left:-600px;
+        top:155px;
     }
     #User h2{
-        font-size: 50px;
+        font-size: 20px;
         color: #283747;
+        position:relative;
+        left:1530px;
+        top:-330px;
+
        
 
         }
     #User span{
         color: crimson;
         font-weight: bold;
+        position:relative;
+        left:0;
+        top:0;
     }
 
     #User button{
@@ -95,8 +107,8 @@
         margin: 10%;
         display:-moz-box;
         position:relative;
-        left:400px;
-        top:-200px;
+        left:1100px;
+        top:-330px;
     }
         
     
